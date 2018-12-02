a8cd8c205af9ad41681cf8dfc25394868da1ac4f
Anshul Chanchlani <anshulchanchlani@gmail.com>
2018-12-02 20:36:13 +0000

    API call to retrieve images

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import Header from '../Header'
+import { getImages } from '../../services/api';
+import './index.scss'
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { images: null }
+    }
+    componentDidMount() {
+
+        let self = this;
+        //Explicitly sending 6 images.
+        getImages()
+            .then((results) => {
+                self.setState({ images: results.slice(0, 6) })
+            })
+    }
     render() {
+        let images = this.state.images;
         return (
-            <Header/>
+           images && images.length>=1 ? <Header />:<h3>Loading...</h3>
         );
     }
 }
