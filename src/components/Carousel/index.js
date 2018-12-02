917d2be3cb49efee122da071a0fe5a0ff3a5277f
Anshul Chanchlani <anshulchanchlani@gmail.com>
2018-12-02 21:32:41 +0000

    Define image navigation and events

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -8,6 +8,33 @@ export default class Carousel extends Component {
     constructor(props) {
         super(props);
         this.state = { activeIndex: 0 };
+        this.nextImage = this.nextImage.bind(this);
+        this.prevImage = this.prevImage.bind(this);
+        this.setCurrentImageIndex = this.setCurrentImageIndex.bind(this)
+    }
+    nextImage(e) {
+        if (this.props.images.length - 1 === this.state.activeIndex) {
+            this.setState({ activeIndex: 0 })
+        } else {
+            this.setState({ activeIndex: this.state.activeIndex + 1 })
+        }
+    }
+
+    prevImage(e) {
+        if (0 === this.state.activeIndex) {
+            this.setState({ activeIndex: this.props.images.length - 1 })
+
+        } else {
+            this.setState({ activeIndex: this.state.activeIndex - 1 })
+        }
+
+    }
+    setCurrentImageIndex(index) {
+
+        if (index !== 'undefined' && index >= 0) {
+            this.setState({ activeIndex: index })
+        }
+ 
     }
     render() {
         let image = this.props.images[this.state.activeIndex]
