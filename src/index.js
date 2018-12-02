c7023b7678ce57fb167e3d451163aac1a6d88652
Anshul Chanchlani <anshulchanchlani@anshuls-mbp.home>
2018-12-02 20:13:56 +0000

    Header component in App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './components/App'
 export default class Root extends Component {
 
     render() {
         return (
-            <h2>Hello</h2>
+            <App/>
         );
     }
 }
